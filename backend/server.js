2839d411730a4d24e57d7f61353f359b400b1fde
Waqasktk456*8* <bscs22f23@namal.edu.pk>
2025-06-08 06:02:14 -0700

    Apply backend test script fix and latest changes after everything is fixed

--- backend/server.js
+++ backend/server.js
@@ -41,5 +41,5 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server  iss runningg on port ${PORT}`);
+  console.log(`Server  is running on port ${PORT}`);
 });
