54f9f2842b27e21babcfb5628116f8cc7b184761
Waqasktk456 <156170851+Waqasktk456@users.noreply.github.com>
2025-06-09 01:34:55 -0700

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Update to production URL if needed
+  baseURL: 'https://whichfood-backend-g7d4fjbth7gbgubz.centralindia-01.azurewebsites.net/', // Update to production URL if needed
   headers: {
     'Content-Type': 'application/json',
   },
